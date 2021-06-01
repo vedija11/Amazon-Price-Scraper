927de353bee456926f1128540f465afffc18ef28
Vedija Jagtap <54568843+vedija11@users.noreply.github.com>
2021-05-31 20:39:12 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -37,13 +37,13 @@ def send_mail():
     server.starttls()           # encrypts connection
     server.ehlo()
 
-    server.login('vedija.jagtap99@gmail.com', 'PR@nvi11')
+    server.login('#EmailID', '#Password')
     subject = 'Price fell down!'
     body = 'Check the amazon link https://www.amazon.in/Boat-Rockerz-400-Bluetooth-Headphones/dp/B01J82IYLW/ref=gbps_img_s-5_9f8e_7e166db3?smid=A14CZOWI0VEHLG&pf_rd_p=b4500f5f-e496-4b18-ab75-623b14149f8e&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_r=R6BV8CKA61WS2QHE114P '
 
     msg = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail('vedija.jagtap99@gmail.com', 'vjagtap@uncc.edu', msg)
+    server.sendmail('#FromEmailId', '#toEmailId', msg)
     print('Hey, Price Drop Email has been sent!!')
 
     server.quit()
